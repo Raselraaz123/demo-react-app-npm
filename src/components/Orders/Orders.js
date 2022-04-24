54abaf1724a8f5d879a8bf5d5d3e21e12cb4ab36
Raselraaz123 <raselraaz91@gmail.com>
2022-04-25 00:03:55 +0600

    add mongodb and modefi site

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-import useCart from '../../hooks/useCart';
-import useProducts from '../../hooks/useProducts';
-import { removeFromDb } from '../../utilities/fakedb';
-import Cart from '../Cart/Cart';
-import ReviewItem from '../ReviewItem/ReviewItem';
-import './Orders.css'
+import useCart from "../../hooks/useCart";
+import { removeFromDb } from "../../utilities/fakedb";
+import Cart from "../Cart/Cart";
+import ReviewItem from "../ReviewItem/ReviewItem";
+import "./Orders.css";
 const Orders = () => {
-  const [products, setProducts] = useProducts(); 
-  const [cart, setCart] = useCart(products)
+
+  const [cart, setCart] = useCart();
   const navigate = useNavigate();
-  
-  const handleRemoveProduct = product => {
-    const rest = cart.filter(pd => pd.id !== product.id)
-    setCart(rest)
-    removeFromDb(product.id)
-  }
+
+  const handleRemoveProduct = (product) => {
+    const rest = cart.filter((pd) => pd._id !== product._id);
+    setCart(rest);
+    removeFromDb(product._id);
+  };
   return (
     <div className="shop-container">
       <div className="review-item-container">
         {cart.map((product) => (
           <ReviewItem
-            key={product.id}
+            key={product._id}
             product={product}
             handleRemoveProduct={handleRemoveProduct}
           ></ReviewItem>
@@ -30,7 +29,9 @@ const Orders = () => {
       </div>
       <div className="cart-container">
         <Cart cart={cart}>
-          <button onClick={() => navigate("/shipment")}>Proceed Shipping</button>
+          <button onClick={() => navigate("/shipment")}>
+            Proceed Shipping
+          </button>
         </Cart>
       </div>
     </div>
